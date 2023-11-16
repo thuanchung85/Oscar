34059ce062049cc74ddfb547eac8caae1e92bdaa
Thuận Chung Lương <thuanchung85@gmail.com>
2023-11-16 12:30:46 +0700

    Initial Commit

---
+++ Oscar/OscarApp.swift
@@ -0,0 +1,17 @@
+//
+//  OscarApp.swift
+//  Oscar
+//
+//  Created by SWEET HOME (^0^)!!! on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct OscarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
